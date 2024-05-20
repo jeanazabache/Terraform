b29e6f71adcfc0f62770b282ca46eb4eaea52ce9
Jean Azabache <>
2024-05-19 22:38:28 -0500

    update file

--- Cloud/AWS-Terraform/EC2_RDS_PEERING/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-################################################################
-#                       EC2
-################################################################
-instance_name = "web-server"
-keypair_oregon = "keypair-o"
-keypair_virginia = "keypair-v"
-windows_server_sg = "windows-server-security-group"
--- Cloud/AWS-Terraform/EC2_RDS_PEERING/variables.tf
+++ Cloud/AWS-Terraform/EC2_RDS_PEERING/variables.tf
@@ -11,18 +11,46 @@ variable "instance_name" {
   description = "Nombre de instancia"
   type = string
 }
+variable "instance_amis" {
+  type = map(string)
 
-variable "keypair_oregon" {
-  description = "Nombre del keypair"
-  type = string
+  default = {
+    "instance1" = "ami-04b70fa74e45c3917",
+    "instance2" = "ami-01175fe07368af8c6"
+  }
 }
 
-variable "keypair_virginia" {
-  description = "Nombre del keypair"
-  type = string
+variable "keypairs" {
+  type = map(string)
+
+  default = {
+    "keypair1" = "keypair-v",
+    "keypair2" = "keypair-o"
+  }
 }
 
+variable "name_instance" {
+  type = map(string)
+  default = {
+    "name1" = "server-linux",
+    "name2" = "server-windows"
+  }
+}
 variable "windows_server_sg" {
   description = "Nombre de virtual machine windows"
   type = string
 }
+variable "type_instance" {
+  type = string
+  description = "Tipo instancia de Web Server"
+  default = "t3.medium"
+}
+variable "password" {
+  type = string
+  description = "Password de Base de Datos"
+  default = "password"
+}
+
+################################################################
+#                       VPC
+################################################################
