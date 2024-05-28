53fb422b64803b6da1357ffd4b2223d3c0e4bce2
Jean Azabache <>
2024-05-27 23:50:41 -0500

    updatte

--- Cloud/AWS-Terraform/NAT_INTANSCE_RDS_LOADBALANCER/vpc.tf
+++ Cloud/AWS-Terraform/NAT_INTANSCE_RDS_LOADBALANCER/vpc.tf
@@ -43,14 +43,14 @@ resource "aws_subnet" "subnet_private_1" {
   provider          = aws.virginia
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
 }
 
 ################## SUBNET 2 PRIVATE ##################
 resource "aws_subnet" "subnet_private_2" {
   provider          = aws.virginia
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
 }
 ################## SUBNET 3 PRIVATE ##################
@@ -58,10 +58,10 @@ resource "aws_subnet" "subnet_private_3" {
   provider          = aws.virginia
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.5.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
 }
 ################## SUBNET 3 PRIVATE ##################
-resource "aws_subnet" "subnet_public_4" {
+resource "aws_subnet" "subnet_private_4" {
   provider          = aws.virginia
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.6.0/24"
@@ -72,7 +72,24 @@ resource "aws_subnet" "subnet_public_4" {
 resource "aws_route_table" "route_table_virginia_public" {
   vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "Route_Table"
+    Name = "Route_Table_Public"
+  }
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway_virginia.id
+  }
+}
+
+################## ROUTE TABLE ##################
+resource "aws_route_table" "route_table_virginia_privado" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "Route_Table_Private"
   }
 
   route {
@@ -80,10 +97,79 @@ resource "aws_route_table" "route_table_virginia_public" {
     gateway_id = "local"
   }
 }
+################## MAIN ASSOCIATION ##################
+resource "aws_main_route_table_association" "main" {
+  provider       = aws.virginia
+  vpc_id         = aws_vpc.vpc.id
+  route_table_id = aws_route_table.route_table_virginia_public.id
+}
+
 
 
 ################## ROUTE TABLE WITH SUBNET - 1 ##################
-resource "aws_route_table_association" "rt-PUBLIC" {
-  subnet_id      = aws_subnet.subnet_public_1.id
+resource "aws_route_table_association" "rt-PUBLIC_1" {
+
+  subnet_id = aws_subnet.subnet_public_1.id
   route_table_id = aws_route_table.route_table_virginia_public.id
 }
+################## ROUTE TABLE WITH SUBNET - 2 ##################
+resource "aws_route_table_association" "rt-PUBLIC_2" {
+
+  subnet_id = aws_subnet.subnet_public_2.id
+  route_table_id = aws_route_table.route_table_virginia_public.id
+}
+
+
+################## ROUTE TABLE WITH SUBNET - 1 ##################
+resource "aws_route_table_association" "rt-PRIVADO_1" {
+
+  subnet_id = aws_subnet.subnet_private_1.id
+  route_table_id = aws_route_table.route_table_virginia_privado.id
+}
+################## ROUTE TABLE WITH SUBNET - 1 ##################
+resource "aws_route_table_association" "rt-PRIVADO_2" {
+
+  subnet_id = aws_subnet.subnet_private_2.id
+  route_table_id = aws_route_table.route_table_virginia_privado.id
+}
+################## ROUTE TABLE WITH SUBNET - 1 ##################
+resource "aws_route_table_association" "rt-PRIVADO_3" {
+
+  subnet_id = aws_subnet.subnet_private_3.id
+  route_table_id = aws_route_table.route_table_virginia_privado.id
+}
+################## ROUTE TABLE WITH SUBNET - 1 ##################
+resource "aws_route_table_association" "rt-PRIVADO_4" {
+
+  subnet_id = aws_subnet.subnet_private_4.id
+  route_table_id = aws_route_table.route_table_virginia_privado.id
+}
+
+/* //  required subnets and their configurations
+variable "required_subnets" {
+  description = "list of subnets required"
+  default     = ["public-1a", "private-1a", "public-1b", "private-1b"]
+}
+
+#create public and provate subnets
+resource "aws_subnet" "subnets" {
+  for_each          = toset(var.required_subnets)
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = lookup(var.subnet_conf[each.key], "cidr")
+  availability_zone = lookup(var.subnet_conf[each.key], "availability_zone")
+
+  # enable public ip addresses in public subnet
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = each.key
+  }
+}
+
+#assosiate public route table with public subnet
+resource "aws_route_table_association" "public" {
+  for_each       = {for name, subnet in aws_subnet.subnets: name => subnet if length(regexall("public-", name)) > 0}   
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.my_public_route_table.id
+} */
