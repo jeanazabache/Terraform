1809e1b8e8042b74bef84fbae68339d594139c6f
Jean Azabache <>
2024-05-27 22:43:47 -0500

    update

--- Cloud/AWS-Terraform/NAT_INTANSCE_RDS_LOADBALANCER/vpc.tf
+++ Cloud/AWS-Terraform/NAT_INTANSCE_RDS_LOADBALANCER/vpc.tf
@@ -23,18 +23,47 @@ resource "aws_internet_gateway" "internet_gateway_virginia" {
   }
 }
 
-################## SUBNET 1 VIRGINIA ##################
+################## SUBNET 1 PUBLIC ##################
 resource "aws_subnet" "subnet_public_1" {
   provider          = aws.virginia
   vpc_id            = aws_vpc.vpc_virginia.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-################## SUBNET 2 VIRGINIA ##################
+################## SUBNET 2 PUBLIC ##################
 resource "aws_subnet" "subnet_public_2" {
   provider          = aws.virginia
   vpc_id            = aws_vpc.vpc_virginia.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+} 
+################## SUBNET 1 PRIVATE ##################
+resource "aws_subnet" "subnet_private_1" {
+  provider          = aws.virginia
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+}
+
+################## SUBNET 2 PRIVATE ##################
+resource "aws_subnet" "subnet_private_2" {
+  provider          = aws.virginia
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+}
+################## SUBNET 3 PRIVATE ##################
+resource "aws_subnet" "subnet_private_3" {
+  provider          = aws.virginia
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+}
+################## SUBNET 3 PRIVATE ##################
+resource "aws_subnet" "subnet_public_4" {
+  provider          = aws.virginia
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = "10.0.6.0/24"
   availability_zone = "us-east-1b"
 }
