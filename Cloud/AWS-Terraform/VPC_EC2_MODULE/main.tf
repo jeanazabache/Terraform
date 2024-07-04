041a05e7b8aa89dc1a6f61732441b1519c0e1ba5
Jean Azabache Medina <jazabachem1@gmail.com>
2024-07-03 21:24:53 -0500

    update

--- Cloud/AWS-Terraform/VPC_EC2_MODULE/main.tf
+++ Cloud/AWS-Terraform/VPC_EC2_MODULE/main.tf
@@ -30,3 +30,59 @@ module "ec2" {
   name_instance_profile = "ssm_instance_profile"
 }
 
+module "security" {
+  source = "./modules/security"
+  vpc_id = module.vpc.vpc_id
+  
+   security_group_rules = [
+    {
+      type        = "ingress"
+      protocol    = "tcp"
+      from_port   = 443
+      to_port     = 443
+      source_security_group = true
+    },
+    {
+      type        = "ingress"
+      protocol    = "tcp"
+      from_port   = 80
+      to_port     = 80
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "ingress"
+      protocol    = "tcp"
+      from_port   = 22
+      to_port     = 22
+      cidr_blocks = ["10.219.230.10/32"]
+    },
+    {
+      type        = "egress"
+      protocol    = "tcp"
+      from_port   = 3306
+      to_port     = 3306
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "egress"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "egress"
+      protocol    = "tcp"
+      from_port   = 80
+      to_port     = 80
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "egress"
+      protocol    = "tcp"
+      from_port   = 443
+      to_port     = 443
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
---
+++ Cloud/AWS-Terraform/VPC_EC2_MODULE/modules/security/main.tf
@@ -0,0 +1,38 @@
+variable "security_group_rules" {
+  description = "List of security group rules"
+  type = list(object({
+    type                     = string
+    protocol                 = string
+    from_port                = number
+    to_port                  = number
+    cidr_blocks              = optional(list(string), [])
+    source_security_group    = optional(bool, false)
+  }))
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Example security group"
+  vpc_id      = "vpc-123456"
+
+  dynamic "ingress" {
+    for_each = [for rule in var.security_group_rules : rule if rule.type == "ingress"]
+    content {
+      from_port                = ingress.value.from_port
+      to_port                  = ingress.value.to_port
+      protocol                 = ingress.value.protocol
+      cidr_blocks              = lookup(ingress.value, "cidr_blocks", null)
+      self                    = ingress.value.source_security_group ? true : false
+    }
+  }
+
+  dynamic "egress" {
+    for_each = [for rule in var.security_group_rules : rule if rule.type == "egress"]
+    content {
+      from_port                = egress.value.from_port
+      to_port                  = egress.value.to_port
+      protocol                 = egress.value.protocol
+      cidr_blocks              = lookup(egress.value, "cidr_blocks", null)
+    }
+  }
+}
